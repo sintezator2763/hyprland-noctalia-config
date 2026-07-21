921a4320f527f10b86c484fab4ac2f4718b483ef
sintezator <cat78suska@gmail.com>
2026-07-21 22:37:28 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,16 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    # nixpkgs.follows НЕ указываем - иначе не будет работать бинарный кэш Cachix
-    # (см. настройку nix.settings в configuration.nix)
     noctalia.url = "github:noctalia-dev/noctalia";
   };
 
   outputs = { self, nixpkgs, noctalia, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = { inherit inputs; }; # прокидывает inputs во все модули
+      specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
       ];
